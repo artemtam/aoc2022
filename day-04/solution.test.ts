f08e4d7d96bda1fc8d788e0ec4463ded45d4b850
artemtam <artemtam1408@gmail.com>
2022-12-04 14:10:26 +0200

    feat: day-4

---
+++ day-04/solution.test.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import { test, expect } from "vitest";
+
+import { solvePart1, solvePart2 } from "./solution";
+
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf8');
+
+test('part 1', () => {
+    expect(solvePart1(input)).toEqual(518);
+});
+
+test('part 2', () => {
+    expect(solvePart2(input)).toEqual(909);
+});
